5e1a8e84b521d114c937fc0d135709a3e7d6ed81
secprepper <thomasjpatterson@users.noreply.github.com>
2025-06-11 21:23:21 -0400

    Switching to MagicPortfolio Template

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import mdx from "@next/mdx";
 
-export default nextConfig;
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  transpilePackages: ["next-mdx-remote"],
+  sassOptions: {
+    compiler: "modern",
+    silenceDeprecations: ["legacy-js-api"],
+  },
+};
+
+export default withMDX(nextConfig);
